32efec9eaad3ffea6b062ee89aff0fcccf4f1731
Pedro Malavasi Betti <malabetti@outlook.com>
2023-01-01 01:59:43 -0300

    Aniversário do tio

---
+++ script.js
@@ -0,0 +1,12 @@
+function muda_main(valor) {
+    var divliv = document.getElementById("box2")
+    if(valor == "tito"){
+        divliv.innerHTML = '<!DOCTYPE html><html><body><h2>TITO</h2><br><br><img src="https://lh3.googleusercontent.com/pw/AL9nZEXbrw7B8TtIdjnFkJIRdpKKqSiS4GNYP3wa0KO5gTNSQfkahXKYbESC2DhDFBNwFclQkMNujWW3lBvEkXspkDYbO0rCsZYiXnXRA0Ls7zI_RogScU7msuwSjVswAnr8q3ahFDcudvmynyGHnBHa4oav3Q=w533-h894-no?authuser=0" height=400px><img src="https://lh3.googleusercontent.com/pw/AL9nZEWvXk4fy-EmNF7hyI2hC5vgmr3gAulOXkVXGkH1FQoGgUHIFNZ7_y6tNnmB4FwyhOsEZSIlr36GY9mIBrvO2A1Hwyqoo4TqnuXLWeRcquOAG2sllhnb5ntotxQEUAZ1aR22qyalRZSLTX3nFxPtWdyqWQ=w1347-h894-no?authuser=0" height=400px width=400px></body></html>'
+    }
+    else if(valor == "og"){
+        divliv.innerHTML = '<!DOCTYPE html><html><body><h2>OG</h2><br><br><img src="https://lh3.googleusercontent.com/pw/AL9nZEW1rc2TAbNfk7TY_PU5yUHLMGo1dk6Sjh-49LSaGonOgDCvOM5IkPumSOzXBdoZyXkAWJQEB_lIbDYMlOyE7LCrWPRU-yc8IhswN3VN3oIgcSVwA_2l6oNV4PWh2ZKDnl7fCb7R2iACpkmH37FSatH20w=w1590-h895-no?authuser=0" height=100px width=200px><img src="https://lh3.googleusercontent.com/pw/AL9nZEWLuDcmRTJh2An9k_LPwLLAyitFuzpuBWNo3JFqZxGKyC1PvPvXoblwbCUldGDqH6SZjJNZXhAPikPEZ3n3oFfjV8T7SsDHoauMjtrYg_7iZSjyGod_XuLqV7yyQUlY0sd4trbN1I71DoWUxQ1sDe_hWQ=w895-h894-no?authuser=0" height=400px width=400px></body></html>'
+    }
+    else{
+        divliv.innerHTML = '<!DOCTYPE html><html><body><h2>ALF</h2><br><br><img src="https://lh3.googleusercontent.com/pw/AL9nZEXr8uHpvTBe30BadgW02eURHxt4aq-_0uwURhi_ewBL5L0eSkWsVg7uT9HdzIe_yM7Vq6NckZEFeByWi93WwhQor1O-Ah7wFYB7saxraGxgwdGRCtzQbzWDKhwjcDEwOdKNFVy-7PY1riqXSPj5M39c6Q=w1193-h894-no?authuser=0" height=300px><img src="https://lh3.googleusercontent.com/pw/AL9nZEW_WuPB8Xplp8iG4ugWpcMklcOC50FNlrldWVmNbpFzsdBlMZjGg5r13pYa_2NiWstw6AlN9U_iMh_opUt5iZJwfy6P0a2mlPBt-9FlY5lhoJByHyQYpMoUEbqLKn0xetjPZZVqMv7dusSQ6N00iL2U5g=w577-h894-no?authuser=0" height=300></body></html>'
+    }
+}
